e13364acc91df6c72923e2b328e9fb9f2159938c
Yulia Ivanova <sheeeellby@gmail.com>
2021-11-15 00:01:03 +0300

    change task1

--- js/task1.js
+++ js/task1.js
@@ -1,29 +1,8 @@
-function getNameSurnamePatronymic(){
-    let name = prompt("Введите ваше имя");
-    let surname = prompt("Введите вашу фамилию");
-    let patronymic = prompt("Введите ваше отчество");
-    while (!name || !surname || !patronymic) {
-        if(!name){
-            name = prompt("Введите ваше имя еще раз");
-        }
-        else if(!surname){
-            surname = prompt("Введите вашу фамилию еще раз");
-        }
-        else{
-            patronymic = prompt("Введите ваше отчество еще раз");
-        } 
+function getValidation(value){
+    while (!value) {
+        value = prompt("Введите еще раз");
     }
-
-    return surname + " " + name + " " + patronymic
-}
-
-function getAge(){
-    let age = +prompt("Введите ваш возраст");
-    let str = "";
-    while(!age){
-        age = +prompt("Введите ваш возраст");
-    }
-    return age;
+    return value
 }
 
 function getGender(){
@@ -32,25 +11,29 @@ function getGender(){
 }
 
 function inRetired(gender, age){
-    let str = "";
     if (gender && age >= 65 || !gender && age >= 55) {
-        str = "Да"
-        return str
+        return "Да"
     } else {
-        str = "Нет"
-        return str
+        return "Нет"
     }
 }
 
-let nameSurnamePatronymic = getNameSurnamePatronymic();
-let age = getAge();
-let gender = getGender();
-let ageInDays = age * 365;
-let ageInFiveYears = age + 5;
-let retired = inRetired(gender, age);
-alert(`Ваше ФИО: ${nameSurnamePatronymic}
-Ваш возраст в годах: ${age}
-Ваш возраст в днях: ${ageInDays}
-Через 5 лет вам будет: ${ageInFiveYears}
-Ваш пол: ${gender}
-Вы на пенсии: ${retired}`);
+function getMessage() {
+    let name = getValidation(prompt("Введите ваше имя"));
+    let surname = getValidation(prompt("Введите вашу фамилию"));
+    let patronymic = getValidation(prompt("Введите ваше отчество"));
+    let age = getValidation(+prompt("Введите ваш возраст"));
+    const gender = getGender();
+    const ageInDays = age * 365;
+    const ageInFiveYears = age + 5;
+    const retired = inRetired(gender, age);
+    alert(`Ваше ФИО: ${name + " " + surname + " " + patronymic}
+    Ваш возраст в годах: ${age}
+    Ваш возраст в днях: ${ageInDays}
+    Через 5 лет вам будет: ${ageInFiveYears}
+    Ваш пол: ${gender}
+    Вы на пенсии: ${retired}`);
+}
+
+getMessage();
+
